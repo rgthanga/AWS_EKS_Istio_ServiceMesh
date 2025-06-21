bff2239e8517fa39a7ae625dd6fa5468fb3f9674
rgthanga <rgthanga@gmail.com>
2025-06-21 09:23:31 +0530

    added

--- eks-tf/IstioBase.tf
+++ eks-tf/IstioBase.tf
@@ -2,16 +2,17 @@
 # helm repo update
 # helm install my-istio-base-release -n istio-system --create-namespace istio/base --set global.istioNamespace=istio-system
 resource "helm_release" "istio_base" {
-  name = "my-istio-base-release"
-
+  name             = "my-istio-base-release"
   repository       = "https://istio-release.storage.googleapis.com/charts"
   chart            = "base"
   namespace        = "istio-system"
   create_namespace = true
   version          = "1.26.1"
 
-  set {
-    name  = "global.istioNamespace"
-    value = "istio-system"
-  }
+  set = [
+    {
+      name  = "global.istioNamespace"
+      value = "istio-system"
+    }
+  ]
 }
--- eks-tf/Istiod.tf
+++ eks-tf/Istiod.tf
@@ -1,34 +1,35 @@
 # helm repo add istio https://istio-release.storage.googleapis.com/charts
 # helm repo update
 # helm install my-istiod-release -n istio-system --create-namespace istio/istiod --set telemetry.enabled=true --set global.istioNamespace=istio-system
-resource "helm_release" "istiod" {
-  name = "my-istiod-release"
 
+resource "helm_release" "istiod" {
+  name             = "my-istiod-release"
   repository       = "https://istio-release.storage.googleapis.com/charts"
   chart            = "istiod"
   namespace        = "istio-system"
   create_namespace = true
-  version          = "1.17.1"
+  version          = "1.26.1"
 
-  set {
-    name  = "telemetry.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "global.istioNamespace"
-    value = "istio-system"
-  }
-
-  set {
-    name  = "meshConfig.ingressService"
-    value = "istio-gateway"
-  }
-
-  set {
-    name  = "meshConfig.ingressSelector"
-    value = "gateway"
-  }
-
-  depends_on = [helm_release.istio_base]
+  set = [
+    {
+      name  = "telemetry.enabled"
+      value = "true"
+    },
+    {
+      name  = "global.istioNamespace"
+      value = "istio-system"
+    },
+    {
+      name  = "meshConfig.ingressService"
+      value = "istio-gateway"
+    },
+    {
+      name  = "meshConfig.ingressSelector"
+      value = "gateway"
+    }
+  ]
+    depends_on = [helm_release.istio_base]
 }
+
+
+

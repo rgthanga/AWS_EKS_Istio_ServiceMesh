a2046e618a455a029ac56cd7e145dbf77bb54a09
rgthanga <rgthanga@gmail.com>
2025-06-21 08:57:27 +0530

    updated k8's

--- eks-tf/HelmProvider.tf
+++ eks-tf/HelmProvider.tf
@@ -28,10 +28,8 @@ data "aws_eks_cluster_auth" "eks" {
   depends_on = [aws_eks_cluster.demo]
 }
 
-provider "helm" {
-  kubernetes {
+provider "kubernetes" {
     host                   = data.aws_eks_cluster.eks.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.eks.token
   }
-}
